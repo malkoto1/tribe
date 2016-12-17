4a2b8130f6390f60236e1c50794403665fc58c4a
Radoslav Asparuhov <asparuhovr@vmware.com>
2016-12-17 15:20:08 +0200

    Some services for start

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,21 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
+class Items(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return 'all items'
 
-class GetItems(Resource):
+    def get(self, field=None, search_str=None):
+        return 'Return product based on match of {} field and {} search string'.format(field, search_str)
+
+
+class Orders(Resource):
     def get(self):
-        return {"Joro": "Kade mi e bazata?"}
+        return 'all orders'
+
+    def get(self, criteria1=None, criteria2=None):
+        return 'all orders between {} and {}'.format(criteria1, criteria2)
+
     def post(self):
-        return "", 200
+        body = request.data.decode('utf-8')
+        print(body)
+        return 200
 
-api.add_resource(HelloWorld, '/')
+
+api.add_resource(Items, '/items', '/items/<field>/<search_str>')
+api.add_resource(Orders,
+                 '/orders',
+                 '/orders/between/<criteria1>/<criteria2>')
 
 if __name__ == '__main__':
     app.run(debug=True)
