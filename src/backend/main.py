a84489dad9f27c3aa9113cdf636f827995fc7a02
Radoslav Asparuhov <asparuhovr@vmware.com>
2016-12-18 14:55:17 +0200

    kor<put a good 1-line overview here!>
    <put your multi-line change description here!>
    
    Bug Number:
    Documentation Notes:
    PrecheckinTest URL: <required>
    QA Notes:
    Review URL: <required>
    Reviewed by: <required>
    Testing Done: <required>

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,16 +1,41 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class Items(Resource):
-    def get(self):
-        return 'all items'
+def get_json_as_dict(file):
+    with open(file, "r") as fp:
+        return json.loads(fp.read())
 
+
+def write_dict_to_file(file, dictionary):
+    with open(file, 'w') as fp:
+        fp.write(json.dumps(dictionary))
+
+
+class Items(Resource):
     def get(self, field=None, search_str=None):
+        if field is None:
+            kor = get_json_as_dict("../../resources/items.json")
+            return str(kor).encode()
         return 'Return product based on match of {} field and {} search string'.format(field, search_str)
+
+    def put(self):
+        body = request.data.decode('utf-8')
+        kor = get_json_as_dict("../../resources/items.json")
+        kor[body['id']] = body
+
+        return 200
+
+    def post(self):
+        body = request.data.decode('utf-8')
+        kor = get_json_as_dict("../../resources/items.json")
+        kor[body['id']] = body
+
+        return 200
 
 
 class Orders(Resource):
@@ -26,10 +51,23 @@ class Orders(Resource):
         return 200
 
 
+class Stats(Resource):
+    def get(self):
+        return 'All stats'
+
+    def get(self, which=None):
+        if which == 'insufficient':
+            return 'All items with not enough quantity'
+        elif which == 'recommendation':
+            return 'All recommendations'
+
+
 api.add_resource(Items, '/items', '/items/<field>/<search_str>')
 api.add_resource(Orders,
                  '/orders',
                  '/orders/between/<criteria1>/<criteria2>')
+
+api.add_resource(Stats, '/stats', '/stats/<which>')
 
 if __name__ == '__main__':
     app.run(debug=True)
